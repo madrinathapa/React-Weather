c78b87553f4ec319e120f933a67b14841377f654
madrinathapa <madrinathapa.rs@gmail.cm>
2017-07-15 20:42:32 -0400

    Integrated OpenWeatherMap API to fetch the temperature.

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,21 @@
+var Axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=ba5ec8f1aa2e1caf18fcfe460f167a3a';
+
+module.exports= {
+  getTemp:function(location){
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return Axios.get(requestUrl).then(function(res){
+      if(res.data.cod && res.data.message){
+        throw new Error(res.data.message);
+      }else{
+        return res.data.main.temp;
+      }
+
+    }, function(res){
+      throw new Error(res.data.message);
+    });
+  }
+}
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var OpenWeatherMap = require('OpenWeatherMap');
 
 var Weather = React.createClass({
   getInitialState:function(){
@@ -10,11 +11,16 @@ var Weather = React.createClass({
     };
   },
   handleSearch: function(location){
-    this.setState({
-      location: location,
-      temp:23
-    });
+    var obj = this;
 
+    OpenWeatherMap.getTemp(location).then(function(temp){
+        obj.setState({
+          location: location,
+          temp:temp
+        });
+      },function (errorMessage){
+        alert(errorMessage);
+      })
   },
   render:function(){
     var {temp,location} = this.state;
