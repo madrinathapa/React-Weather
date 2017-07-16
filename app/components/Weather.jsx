cbd92b81cf15fe386e6b42e6b83487a154421ed1
madrinathapa <madrinathapa.rs@gmail.cm>
2017-07-15 20:14:47 -0400

    Implemented data flow from container component to presentation component

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -3,12 +3,27 @@ var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 
 var Weather = React.createClass({
+  getInitialState:function(){
+    return{
+      location:'Miami',
+      temp:89
+    };
+  },
+  handleSearch: function(location){
+    this.setState({
+      location: location,
+      temp:23
+    });
+
+  },
   render:function(){
+    var {temp,location} = this.state;
+
     return (
       <div>
         <h2>Weather component goes here</h2>
-        <WeatherForm></WeatherForm>
-        <WeatherMessage/>
+        <WeatherForm onSearch={this.handleSearch}></WeatherForm>
+        <WeatherMessage temp={temp} location={location}/>
       </div>
     )
   }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,9 +2,11 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
   render:function(){
+    var {temp,location} = this.props;
+
     return(
       <div>
-        <h4>The temp is</h4>
+        <h4>The temp in {location} is {temp}</h4>
       </div>
     );
   }
