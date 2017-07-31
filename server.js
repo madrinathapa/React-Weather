ef0c88963a9081d73bd8259da0df2e32b4a5aeeb
Madrina Thapa <mthapa@indiana.edu>
2017-07-31 17:08:26 -0400

    Fixed server for local

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto']=='http'){
-    next();
-  }else{
+  if(req.headers['x-forwarded-proto']=='https'){
     res.redirect('http://'+req.hostname+req.url);
+  }else{
+    next();
   }
 });
 
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
+app.listen(port, function(){
   console.log("Server is up! : "+port);
 })
